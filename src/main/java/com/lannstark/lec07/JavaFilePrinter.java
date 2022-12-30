b9e704ff61a05a2fc84e7c4f82c2ecd041731429
SeYeol_Park <79959576+SeYeol00@users.noreply.github.com>
2022-12-30 21:49:55 +0900

    12/30

--- src/main/java/com/lannstark/lec07/JavaFilePrinter.java
+++ src/main/java/com/lannstark/lec07/JavaFilePrinter.java
@@ -1,12 +1,15 @@
 package com.lannstark.lec07;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class JavaFilePrinter {
 
   public void readFile(String path) throws IOException {
+//    File currentFile = new File(".");
+//    File file = new File(currentFile.getAbsoluteFile() + "/a.txt");
     try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
       System.out.println(reader.readLine());
     }
